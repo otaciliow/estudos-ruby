b729afd61e60f4058c3c578eae3e50c0f7a6bc1a
otaciliow <otacilio.dml@hotmail.com>
2026-01-31 13:08:22 -0300

    feat: add menu and more options

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,41 @@
 puts "Calculadora em Ruby (Soma)"
 
-print "Digite um número: "
-num1 = gets.chomp().to_f
+puts "Selecione a operação que deseja utilizar:"
+puts "1 - Soma"
+puts "2 - Subtração"
+puts "3 - Multiplicação"
+puts "4 - Divisão"
+puts "5 - Sair\n "
 
-print "Digite o segundo número: "
-num2 = gets.chomp().to_f
+opcao = gets.chomp()
 
-puts "Resultado: #{num1 + num2}"
+if opcao != "5"
+
+    print "Digite o primeiro valor: "
+    num1 = gets.chomp().to_f
+
+    print "Digite o segundo valor: "
+    num2 = gets.chomp().to_f
+
+    resultado = 
+        case opcao
+        when "1"
+            num1 + num2
+        when "2"
+            num1 - num2
+        when "3"
+            num1 * num2
+        when "4"
+            num1 / num2
+        else
+            puts "Opção inválida!"
+        end
+
+    puts "Resultado: #{resultado}"
+    puts
+end
+
+if opcao == "5"
+    puts "Programa encerrado!"
+    return
+end
