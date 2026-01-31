3f4ec3e1d235a375e859326b08b5430e23e8aa48
otaciliow <otacilio.dml@hotmail.com>
2026-01-31 12:56:05 -0300

    first commit

---
+++ calculator.rb
@@ -0,0 +1,9 @@
+puts "Calculadora em Ruby (Soma)"
+
+print "Digite um número: "
+num1 = gets.chomp().to_f
+
+print "Digite o segundo número: "
+num2 = gets.chomp().to_f
+
+puts "Resultado: #{num1 + num2}"
