b4706c91985322d8b341679338fa10c3ff683d8a
otaciliow <otacilio.dml@hotmail.com>
2026-02-02 15:56:59 -0300

    update: now try count is shown dynamically, based on number of attempts

--- guess_game.rb
+++ guess_game.rb
@@ -6,12 +6,19 @@ guess_count = 0
 guess_limit = secret_word.length / 2
 out_of_guesses = false
 
-puts "A palavra tem #{secret_word.length} letras. Você tem #{guess_limit} tentativas..."
+puts "A palavra tem #{secret_word.length} letras."
 puts
 
 while guess != secret_word and !out_of_guesses
+  
+  if guess_count == 0
+    mensagem = "Digite a palavra: "
+  else
+    mensagem = "Errou! Você ainda tem #{guess_limit - guess_count} tentativa(s).\nDigite a palavra: "
+  end
+
   if guess_count < guess_limit
-    print ("Digite a palavra: ")
+    print mensagem
     guess = gets.chomp()
     guess_count += 1
   else
