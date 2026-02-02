1f5d0d35252d72962a07f8a1fa5a508ee0ad49a8
otaciliow <otacilio.dml@hotmail.com>
2026-02-02 19:56:45 -0300

    feat: add user interaction via terminal

--- exponent_calculator.rb
+++ exponent_calculator.rb
@@ -6,4 +6,17 @@ def pow(base_num, pow_num)
   return result
 end
 
-puts pow(7, 2)
+puts "------------------------------------------"
+puts "| Cálculo de expoente fracionário (raiz) |"
+puts "------------------------------------------"
+
+print "Digite o número base: "
+num1 = gets.chomp().to_i
+
+print "Digite o número expoente: "
+num2 = gets.chomp().to_i
+
+resultado = pow(num1, num2)
+
+puts
+puts "Resultado: #{resultado}"
