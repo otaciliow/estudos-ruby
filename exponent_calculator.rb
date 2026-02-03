71aa96466e7487a7d901e647371d7a1c0e5460b2
otaciliow <otacilio.dml@hotmail.com>
2026-02-03 11:55:14 -0300

    feat: add error handling to code

--- exponent_calculator.rb
+++ exponent_calculator.rb
@@ -1,22 +1,47 @@
-def pow(base_num, pow_num)
+def pow(base_num, exponent)
+  raise ArgumentError, "Expoente negativo não é suportado!" if exponent < 0
+  raise ZeroDivisionError, "Zero elevado a expoente negativo é indefinido!" if base_num == 0 and exponent < 0
+
   result = 1
-  pow_num.times do
+
+  exponent.times do
     result = result * base_num
   end
+
   return result
 end
 
-puts "------------------------------------------"
-puts "| Cálculo de expoente fracionário (raiz) |"
-puts "------------------------------------------"
+def ask_integer(message)
+    loop do
+      print message
+      input = gets
 
-print "Digite o número base: "
-num1 = gets.chomp().to_i
+      raise EOFError, "Entrada encerrada pelo usuário" if input.nil?
 
-print "Digite o número expoente: "
-num2 = gets.chomp().to_i
+      begin
+        return Integer(input.chomp)
+      rescue ArgumentError
+        puts "Entrada inválida! Digite um número inteiro."
+      end
+    end
+end
 
-resultado = pow(num1, num2)
+puts "--------------------------------------"
+puts "| Cálculo de expoente inteiro (raiz) |"
+puts "--------------------------------------"
 
-puts
-puts "Resultado: #{resultado}"
+begin
+  num1 = ask_integer("Digite o número base: ")
+  num2 = ask_integer("Digite o número expoente: ")
+
+  resultado = pow(num1, num2)
+
+  puts
+  puts "Resultado: #{resultado}"
+rescue ArgumentError, ZeroDivisionError => e
+  puts
+  puts"Erro: #{e.message}"
+rescue EOFError => e
+  puts
+  puts e.message
+end
